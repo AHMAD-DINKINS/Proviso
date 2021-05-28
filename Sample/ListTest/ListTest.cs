4b1cdcb350d782c275b521e9d07faa979cda3706
Angello Astorga <aastorg2@illinois.edu>
2021-05-27 21:17:28 -0500

    adding example project to analyze

---
+++ Sample/ListTest/ListTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SampleList;
+using Microsoft.Pex.Framework;
+using Microsoft.Pex.Framework.Settings;
+using Microsoft.Pex.Framework.Exceptions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PexAPIWrapper;
+
+namespace SampleList.Test
+{
+    [PexClass(typeof(SampleList.List))]
+    [TestClass]
+    public partial class ListTest
+    {
+        [PexMethod]
+        public void CheckSample([PexAssumeNotNull]List l, int x)
+        {
+            
+            int OldCount = l.Count();
+            PexObserve.ValueForViewing("$input_x", x);
+            PexObserve.ValueForViewing("$input_Count", OldCount);
+
+            PexAssume.IsTrue(true);
+            
+
+            l.addToEnd(x);
+
+            //NotpAssume.IsTrue((oldCount + 1) == l.Count());
+            PexAssert.IsTrue((OldCount + 1) == l.Count());
+        } 
+    }
+}
