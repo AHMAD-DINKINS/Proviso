1b11818da24c58de182262233f021e33bf95ba47
Angello Astorga <aastorg2@illinois.edu>
2021-06-18 16:19:14 -0500

    provindin outline for ahmad in runTeacher to parse feature value into FeatureVectors

--- evosuite.py
+++ evosuite.py
@@ -26,24 +26,22 @@ class Evosuite(Teacher):
     def __init__(self, binary:str, otherArgs:List[str]):
         Teacher.__init__(self, binary, otherArgs)
         
-    def GetExecCommand(self, testDll, testMethod, testNamespace, testType):
-        cmd_exec =[self.binary,testDll ,'/membernamefilter:M:'+testMethod+'!', '/methodnamefilter:'+testMethod+'!','/namespacefilter:'+testNamespace +'!', '/typefilter:'+testType+'!']
-        cmd_exec.extend(['/ro:'+self.ro, '/rn:'+self.rn,'/rl:'+self.pexReportFormat])
-        cmd_exec.extend(self.arguments)
-        
-        return cmd_exec  
+    def GetExecCommand(self, testDll, testMethod, testNamespace, testType, kindOfData):
+        return self.binary+ " "+ kindOfData
 
     def RunTeacher(self, problem, PUTName, precisFeatureList, preOrPost, kindOfData) -> List[FeatureVector]:
-        args = self.GetExecCommand(problem.testDll, PUTName, problem.testNamespace, problem.testClass)
-        argsStr = (' ').join(args)
-        print(argsStr)
-        pexOutput = command_runner.runCommand((' ').join(args))
+        
+        args = self.GetExecCommand(problem.testDll, PUTName, problem.testNamespace, problem.testClass , kindOfData)
+        changeDirOut = command_runner.runCommand("cd ../onboard")
+        
+        print(args)
+        evoOut = command_runner.runCommand(args)
         
         print("evosuite output##############")
-        print(pexOutput)
-        self.reportBaseLocation = problem.testDebugFolder
-        self.reportLocation = os.path.join(self.reportBaseLocation, self.ro, self.rn, "report.per")
-        self.testsLocation = os.path.join(self.reportBaseLocation, self.ro, self.rn, "tests")
+        print(evoOut)
+        
+        featVectors: List[FeatureVector] = None # TODO: Ahmad learn from this guy parseReportForPrecondition in pex.py
 
-        featVectors: List[FeatureVector] = self.ReadOutput(problem.testDebugFolder, precisFeatureList, preOrPost, kindOfData)
+        changeDirOut = command_runner.runCommand("cd -")
+        print(changeDirOut)
         return featVectors
