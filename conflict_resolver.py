a99584b1aae7671be62a10ba2e71c261c972e35f
Angello Astorga <aastorg2@illinois.edu>
2021-06-21 16:01:52 -0500

    adding type information

--- conflict_resolver.py
+++ conflict_resolver.py
@@ -2,6 +2,7 @@
 
 
 
+from precis_feature import PrecisFeature
 from feature_vector import FeatureVector
 from typing import List
 
@@ -9,7 +10,7 @@ class ConflictResolver:
     def __init__(self):
         self.samples:List[FeatureVector] = []
 
-    def removeDuplicates(self,fvs):  
+    def removeDuplicates(self,fvs:List[FeatureVector]):  
         uniquePairs = set()
         for index in range(0, len(fvs)):
             uniquePairs.add((fvs[index],fvs[index].counterexampleLabel))
@@ -18,11 +19,21 @@ class ConflictResolver:
 
         return list(unique)
 
+    def checkNoConflict(self, fvs:List[FeatureVector]):
+        table ={}
+        for fv in fvs:
+            if  not fv in table:
+                table[fv] = fv.counterexampleLabel
+            else:
+                raise Exception( "conflict already in existing samples -- this cannot happen error!")
+        return table.items
 
     #res = [x for x in range(len(lst)) if lst[x] == 10]
     def addSamplesAndResolveConflicts(self, fvs:List[FeatureVector]):
         
         uniquefvs:List[FeatureVector] = self.removeDuplicates(fvs)
+        #noConflicfvs = self.checkNoConflict(uniquefvs)
+
         #TODO: move this to constructor
         if len(self.samples) == 0:
             self.samples.extend(uniquefvs)
@@ -70,3 +81,37 @@ class ConflictResolver:
 
 
 
+if __name__ == "__main__":
+    intx = PrecisFeature( False, "Oldx", "INT", False)
+    inty = PrecisFeature( False, "Oldy", "INT", False)
+    feats = [intx, inty]
+    
+    #Test case 1: conflicting fvs added in two different calls to addSamples
+    
+    # fv1 =FeatureVector(feats, ['2','1'], False, "NEG")
+    # fv2 =FeatureVector(feats, ['2','1'], True, "NEG")
+    # fv3 =FeatureVector(feats, ['3','4'], True, "POS")
+    # fv4 =FeatureVector(feats, ['3','4'], True, "POS")
+    # fv5 =FeatureVector(feats, ['5','6'], True, "POS")
+
+    # r1 = ConflictResolver()
+    # r1.addSamplesAndResolveConflicts([fv5,fv2])
+    # interFvs = r1.getSamples()
+    # assert len(interFvs) == 2 # Adding two different vectors
+
+    # r1.addSamplesAndResolveConflicts([fv1]) #now adding a fv that will cause conflict.
+    # interFvs1 = r1.getSamples()
+    # assert len(interFvs1) == 2
+
+    #Test case 2: adding conflicting fvs at the same time
+    fv1 =FeatureVector(feats, ['2','1'], False, "NEG")
+    fv2 =FeatureVector(feats, ['2','1'], True, "NEG")
+    fv3 =FeatureVector(feats, ['3','4'], True, "POS")
+    fv4 =FeatureVector(feats, ['3','4'], True, "POS")
+    fv5 =FeatureVector(feats, ['5','6'], True, "POS")
+
+    r2 = ConflictResolver()
+    r2.addSamplesAndResolveConflicts([fv1,fv2])
+    interFvs = r2.getSamples()
+    assert len(interFvs) == 1 
+    pass
