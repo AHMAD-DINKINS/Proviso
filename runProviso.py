6e54ca7ade99fbca17eabcccb8cbeafadb654a3a
Angello Astorga <aastorg2@illinois.edu>
2021-07-14 13:30:06 -0500

    should be empty list and not none

--- runProviso.py
+++ runProviso.py
@@ -95,8 +95,8 @@ def learnPreconditionForExceptions(problem: Problem, putName: str, mut:str):
     resolver = ConflictResolver()
     fvs=[]
     allPreconditions = []
-    negFv: List[FeatureVector] = None
-    negFvRand: List[FeatureVector] = None
+    negFv: List[FeatureVector] = []
+    negFvRand: List[FeatureVector] = []
     while True:
         
         if precondition != 'false':
